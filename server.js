864d6798a8bf3d8a0b3631059b42d6edc1e60157
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-12-04 16:46:31 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.JWT_SECRET;
 
 app.use(express.json());
-app.use(cors({ methods: ["GET", "POST"], allowedHeaders: ["Content-Type"] }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 mongoose.connect(process.env.MONGO_URI);
 
