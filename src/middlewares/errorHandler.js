23774b9b19a2168e9c4f11002360978d23282534
S0dxu <159276909+S0dxu@users.noreply.github.com>
2026-03-05 22:40:18 +0100

    Delete src directory

--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error(err);
-  if (err.code === 11000) {
-    return res.status(400).json({ message: "Duplicate key" });
-  }
-  res.status(500).json({ message: "Internal Server Error" });
-};
